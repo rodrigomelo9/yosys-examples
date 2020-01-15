1802de6c53352829b221148b2984b088c02fa877
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2020-01-15 13:35:12 -0300

    Modified implement.py to specify which tool to use with argpase

--- examples/xilinx/implement.py
+++ examples/xilinx/implement.py
@@ -1,26 +1,35 @@
+import argparse
 from glob import glob
 
 from fpga.project import Project
 
-TOOLS = ['ise', 'vivado']
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--tool',
+    '-t',
+    default='ise'
+)
+args = parser.parse_args()
 
 verilogs = glob('temp/**/*.v', recursive=True)
+filesqty = len(verilogs)
 
-print('* Collected Verilog files: %s' % len(verilogs))
+print('* Collected Verilog files: %s' % filesqty)
 
-fh = open('build/errors.txt', 'w')
+fh = open('{}.log'.format(args.tool), 'w')
 fileno = 1
 
-for verilog in verilogs:
-    for tool in TOOLS:
-        print('* Processing %d / %d with %s' % (fileno, len(verilogs), tool))
-        PRJ = Project(tool)
-        PRJ.set_outdir('build/%s' % tool)
-        PRJ.add_files(verilog)
-        PRJ.set_top(verilog)
-        try:
-            PRJ.generate(to_task='syn')
-        except Exception as e:
-            fh.write('* {} - {}'.format(tool, verilog))
-            fh.write('{} ({})'.format(type(e).__name__, e))
+for filename in verilogs:
+    info = '* {} - {} ({}/{})'.format(args.tool, filename, fileno, filesqty)
+    print(info)
+    PRJ = Project(args.tool)
+    dirname = filename.replace('/', '_').replace('.v', '')
+    PRJ.set_outdir('build/{}'.format(dirname))
+    PRJ.add_files(filename)
+    PRJ.set_top(filename)
+    try:
+        PRJ.generate(to_task='syn')
+    except Exception as e:
+        fh.write(info + '\n\n')
+        fh.write('{} ({})\n\n'.format(type(e).__name__, e))
     fileno += 1
